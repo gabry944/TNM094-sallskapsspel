f59284b814a5284839de29544d1cbbae1d683d0d
gabry944 <gabry944@student.liu.se>
2015-03-05 14:03:43 +0100

    basic app for developmed with metaio

---
+++ Sprint1_metaio/src/com/google/sprint1/MainActivity.java
@@ -0,0 +1,68 @@
+package com.google.sprint1;
+
+import java.io.IOException;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.sprint1.R;
+
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.tools.io.AssetsManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	/**
+	 * This task extracts all the assets to an external or internal location
+	 * to make them accessible to Metaio SDK
+	 */
+	private class AssetsExtracter extends AsyncTask<Integer, Integer, Boolean>
+	{		
+		@Override
+		protected Boolean doInBackground(Integer... params) 
+		{
+			try 
+			{
+				// Extract all assets except Menu. Overwrite existing files for debug build only.
+				final String[] ignoreList = {"Menu", "webkit", "sounds", "images", "webkitsec"};
+				AssetsManager.extractAllAssets(getApplicationContext(), "", ignoreList, BuildConfig.DEBUG);
+			} 
+			catch (IOException e) 
+			{
+				MetaioDebug.printStackTrace(Log.ERROR, e);
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
