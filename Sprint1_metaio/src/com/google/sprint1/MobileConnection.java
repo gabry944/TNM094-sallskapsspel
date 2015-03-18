606aa0e6edad7080637291172cd31b611221e1ba
MichaelSjostrom <micke_moj@hotmail.com>
2015-03-18 12:12:46 +0100

    added NsdHelper and MobileConnection

---
+++ Sprint1_metaio/src/com/google/sprint1/MobileConnection.java
@@ -0,0 +1,12 @@
+package com.google.sprint1;
+
+import android.os.Handler;
+
+public class MobileConnection {
+	
+	private Handler mUpdateHandler;
+	
+	public MobileConnection(Handler handler){
+		mUpdateHandler = handler;
+	}
+}
