172759baaee8a437853f9c9c9f4d65cd70d0115e
Jens Jakobsson <jens.j.jakobsson@gmail.com>
2015-03-20 11:52:11 +0100

    music binding

--- Sprint1_metaio/src/com/google/sprint1/MainActivity.java
+++ Sprint1_metaio/src/com/google/sprint1/MainActivity.java
@@ -6,14 +6,26 @@ import android.view.WindowManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
 
 import com.google.sprint1.R;
 
 /** 
  * MainActivity handle the main menu and Its buttons
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+	implements
+		ServiceConnection
 
+	{
+	
+	private boolean mIsBound = false;
+	
+	private MusicService mServ;
+	
 	/**Called at opening, drawing main layout */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +33,13 @@ public class MainActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+		
+		Intent music = new Intent(this, MusicService.class);
+		startService(music);
+		
+		doBindService();
+		
+		//mServ.start();
 	}
 
 	
@@ -38,5 +57,45 @@ public class MainActivity extends Activity {
 		startActivity(intentSettings);
 
 	}
+	
+	// interface connection with the service activity
+		public void onServiceConnected(ComponentName name, IBinder binder)
+		{
+			mServ = ((MusicService.ServiceBinder) binder).getService();
+		}
+		
+		public void onServiceDisconnected(ComponentName name)
+		{
+			mServ = null;
+		}
+		
+		// local methods used in connection/disconnection activity with service.
+		
+		public void doBindService()
+		{
+			// activity connects to the service.
+	 		Intent intent = new Intent(this, MusicService.class);
+			bindService(intent, this, Context.BIND_AUTO_CREATE);
+			mIsBound = true;
+		}
+		
+		public void doUnbindService()
+		{
+			// disconnects the service activity.
+			if(mIsBound)
+			{
+				unbindService(this);
+	      		mIsBound = false;
+			}
+		}
+		// when closing the current activity, the service will automatically shut down(disconnected).
+		@Override
+		public void onDestroy()
+		{
+			super.onDestroy();
+			
+			doUnbindService();
+		}	
+	
 
 }
--- Sprint1_metaio/src/com/google/sprint1/MusicService.java
+++ Sprint1_metaio/src/com/google/sprint1/MusicService.java
@@ -1,5 +1,6 @@
 package com.google.sprint1;
 
+import com.google.sprint1.R;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -7,16 +8,32 @@ import android.media.MediaPlayer.OnErrorListener;
 import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
-import com.google.sprint1.R;
+import android.widget.MediaController;
 
-public class MusicService extends Service  implements MediaPlayer.OnErrorListener{
-
+//---------------------------------------------------------------------------------------
+public class MusicService extends Service 
+	implements
+		MediaPlayer.OnErrorListener,
+		
+		// Interface used by the visual representation of the player controls.
+		MediaController.MediaPlayerControl,
+		
+		// implemented to possibly upgrade the media player interface.
+		MediaPlayer.OnBufferingUpdateListener
+	
+	{
+	// Connect service who to call onBind
     private final IBinder mBinder = new ServiceBinder();
+    
+    // Instance of media player
     MediaPlayer mPlayer;
-    private int length = 0;
-
+    
+    // Saves the data buffer of the media player (used by MediaController).
+    private int mBuffer = 0;
+    
     public MusicService() { }
-
+    
+    // Called by the interface ServiceConnected when calling the service
     public class ServiceBinder extends Binder {
      	 MusicService getService()
     	 {
@@ -25,75 +42,31 @@ public class MusicService extends Service  implements MediaPlayer.OnErrorListene
     }
 
     @Override
-    public IBinder onBind(Intent arg0){return mBinder;}
+    public IBinder onBind(Intent context)
+    {
+    	return mBinder;
+    }
 
     @Override
-    public void onCreate (){
-	  super.onCreate();
-
-       mPlayer = MediaPlayer.create(this, R.raw.jingle);
-       mPlayer.setOnErrorListener(this);
-
-       if(mPlayer!= null)
-        {
-        	mPlayer.setLooping(true);
-        	mPlayer.setVolume(100,100);
-        }
-
-
-        mPlayer.setOnErrorListener(new OnErrorListener() {
-
-	  public boolean onError(MediaPlayer mp, int what, int
-          extra){
-
-			onError(mPlayer, what, extra);
-			return true;
-		}
-    	  });
+    public void onCreate () {
+    	super.onCreate();
+    	
+    	create();
 	}
 
     @Override
-	public int onStartCommand (Intent intent, int flags, int startId)
-	{
-         mPlayer.start();
+	public int onStartCommand (Intent intent, int flags, int startId) {
          return START_STICKY;
 	}
 
-	public void pauseMusic()
-	{
-		if(mPlayer.isPlaying())
-		{
-			mPlayer.pause();
-			length=mPlayer.getCurrentPosition();
-
-		}
-	}
-
-	public void resumeMusic()
-	{
-		if(mPlayer.isPlaying()==false)
-		{
-			mPlayer.seekTo(length);
-			mPlayer.start();
-		}
-	}
-
-	public void stopMusic()
-	{
-		mPlayer.stop();
-		mPlayer.release();
-		mPlayer = null;
-	}
-
 	@Override
-	public void onDestroy ()
-	{
+	public void onDestroy () {
 		super.onDestroy();
 		if(mPlayer != null)
 		{
-		try{
-		 mPlayer.stop();
-		 mPlayer.release();
+			try{
+				 mPlayer.stop();
+				 mPlayer.release();
 			}finally {
 				mPlayer = null;
 			}
@@ -101,8 +74,8 @@ public class MusicService extends Service  implements MediaPlayer.OnErrorListene
 	}
 
 	public boolean onError(MediaPlayer mp, int what, int extra) {
-
 		Toast.makeText(this, "music player failed", Toast.LENGTH_SHORT).show();
+		
 		if(mPlayer != null)
 		{
 			try{
@@ -113,5 +86,107 @@ public class MusicService extends Service  implements MediaPlayer.OnErrorListene
 			}
 		}
 		return false;
+	}
+	
+	public void create()
+	{
+    	mPlayer = MediaPlayer.create(this, R.raw.jingle);
+    	mPlayer.setOnErrorListener(this);
+
+    	mPlayer.setOnErrorListener(new OnErrorListener() {
+    		public boolean onError(MediaPlayer mp, int what, int extra) {
+    			onError(mPlayer, what, extra);
+    			return true;
+    		}
+    	});
+	}
+	
+	@Override
+	public boolean canPause() {
+		return true;
+	}
+
+	@Override
+	public boolean canSeekBackward() {
+		return true;
+	}
+
+	@Override
+	public boolean canSeekForward() {
+		return true;
+	}
+
+	@Override
+	public int getBufferPercentage() {
+		return mBuffer;
+	}
+
+	@Override
+	public int getCurrentPosition() {
+		return mPlayer.getCurrentPosition();
+	}
+
+	@Override
+	public int getDuration() {
+		return mPlayer.getDuration();
+	}
+
+	@Override
+	public boolean isPlaying() {
+		return mPlayer.isPlaying();
+	}
+
+	@Override
+	public void pause() {
+		if(mPlayer != null && isPlaying())
+		{
+			mPlayer.pause();
+		}
+	}
+
+	@Override
+	public void seekTo(int pos) {
+		mPlayer.seekTo(pos);
+	}
+	
+	public void resume()
+	{
+		if(isPlaying() == false)
+		{
+			seekTo(getCurrentPosition());
+			start();
+		}
+	}
+	
+	@Override
+	public void start() {
+		if (mPlayer == null) create();
+		
+		Thread th = new Thread(new Runnable() {
+			@Override
+			public void run()
+			{
+				mPlayer.start();
+			}
+		});
+		th.start();
+	}
+	
+	public void stop() {
+		mPlayer.stop();
+		mPlayer.release();
+		mPlayer = null;
+	}
+
+	@Override
+	public void onBufferingUpdate(MediaPlayer mp, int percent) {
+		mBuffer = percent;
+		
+	}
+
+	@Override
+	public int getAudioSessionId() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
