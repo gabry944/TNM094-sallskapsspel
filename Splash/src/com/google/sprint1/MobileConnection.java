3ebd0bf0f17c522a166cac54195b8cfdc71fd7f6
MichaelSjostrom <micke_moj@hotmail.com>
2015-04-10 10:35:16 +0200

    moved initialization of out and in stream

--- Splash/src/com/google/sprint1/MobileConnection.java
+++ Splash/src/com/google/sprint1/MobileConnection.java
@@ -165,13 +165,15 @@ public class MobileConnection {
 					if (getSocket() == null) {
 						setSocket(new Socket(mAddress, PORT));
 						Log.d(CLIENT_TAG, "Client-side socket initialized.");
-						outStream = new ObjectOutputStream(getSocket().getOutputStream());
-						inStream = new ObjectInputStream(getSocket().getInputStream());
+						
 					} else {
 						Log.d(CLIENT_TAG,
 								"Socket already initialized. skipping!");
 					}
-
+					
+					outStream = new ObjectOutputStream(getSocket().getOutputStream());
+					inStream = new ObjectInputStream(getSocket().getInputStream());
+					
 					mRecThread = new Thread(new ReceivingThread());
 					mRecThread.start();
 
