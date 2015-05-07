e4ddac25401d995e4664fe32aee8eabb2a90b92a
Jens Jakobsson <jens.j.jakobsson@gmail.com>
2015-05-07 10:45:21 +0200

    Revert "load class"
    This reverts commit e0cfeee1072e68f42c08c864b2a36f7c37123a60.

--- Splash/src/com/google/sprint1/Sound.java
+++ Splash/src/com/google/sprint1/Sound.java
@@ -4,37 +4,9 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
-import android.media.SoundPool.OnLoadCompleteListener;
-import android.util.Log;
 
 public class Sound {
 	
-	static boolean loaded = false;
-	
-	public static int[] loadSound(Context context){
-		
-		SoundPool soundPool;
-		soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-		int soundIds[] = new int[10];
-		soundIds[0] = soundPool.load(context, R.raw.ping, 1);
-		
-		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener(){
-
-			@Override
-			public void onLoadComplete(SoundPool soundPool, int sampleId,
-					int status) {
-				loaded = true;
-				
-			}
-			
-		});
-		
-		Log.e("Test", "soundID before = " + soundIds[0]);
-		Log.e("Test", "Loaded = " + loaded);
-		
-		return soundIds;
-		
-		}
 	
 	public static void playBackgroundMusic(Context context){
 	
@@ -47,10 +19,10 @@ public class Sound {
 	
 	}
 	
-	public static void playsoundEffect(int soundID){
+	public static void playsoundEffect(Context context){
 		
 		
-		//should use soundpool insteed of mediaplayer
+		//Vill använda SoundPool istället för mediaplayer
 		/*SoundPool sp;
 		
 		sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
@@ -59,22 +31,13 @@ public class Sound {
 		soundIds[0] = sp.load(context, R.raw.ping, 1);
 		
 		sp.play(soundIds[0], 1, 1, 1, 0, (float) 1.0);*/
-		/*
+		
 		MediaPlayer backgroundMusic;
 		backgroundMusic = MediaPlayer.create(context, R.raw.ping);
 		
 		backgroundMusic.setLooping(false);
 		backgroundMusic.setVolume(10.0f, 3.0f);
 		backgroundMusic.start();
-		*/
-		
-		SoundPool soundPool;
-		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-		
-		Log.e("Test", "soundID play = " + soundID);
-		
-		soundPool.play(soundID, 1, 1, 1, 0, 1.0f);
-		
 	}
 
 }
