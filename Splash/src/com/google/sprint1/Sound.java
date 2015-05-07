f354c26d714b74bb124bbff393a4abb5dfd1462d
Jens Jakobsson <jens.j.jakobsson@gmail.com>
2015-05-07 10:50:10 +0200

    load function

--- Splash/src/com/google/sprint1/Sound.java
+++ Splash/src/com/google/sprint1/Sound.java
@@ -4,9 +4,37 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+import android.util.Log;
 
 public class Sound {
 	
+	static boolean loaded = false;
+	
+	public static int[] loadSound(Context context){
+		
+		SoundPool soundPool;
+		soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+		int soundIds[] = new int[10];
+		soundIds[0] = soundPool.load(context, R.raw.ping, 1);
+		
+		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener(){
+
+			@Override
+			public void onLoadComplete(SoundPool soundPool, int sampleId,
+					int status) {
+				loaded = true;
+				
+			}
+			
+		});
+		
+		Log.e("Test", "soundID before = " + soundIds[0]);
+		Log.e("Test", "Loaded = " + loaded);
+		
+		return soundIds;
+		
+		}
 	
 	public static void playBackgroundMusic(Context context){
 	
@@ -19,7 +47,7 @@ public class Sound {
 	
 	}
 	
-	public static void playsoundEffect(Context context){
+	public static void playsoundEffect(int soundID){
 		
 		
 		//Vill använda SoundPool istället för mediaplayer
@@ -32,12 +60,19 @@ public class Sound {
 		
 		sp.play(soundIds[0], 1, 1, 1, 0, (float) 1.0);*/
 		
-		MediaPlayer backgroundMusic;
+		/*MediaPlayer backgroundMusic;
 		backgroundMusic = MediaPlayer.create(context, R.raw.ping);
 		
 		backgroundMusic.setLooping(false);
 		backgroundMusic.setVolume(10.0f, 3.0f);
-		backgroundMusic.start();
+		backgroundMusic.start();*/
+		
+		SoundPool soundPool;
+		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		
+		Log.e("Test", "soundID play = " + soundID);
+		
+		soundPool.play(soundID, 1, 1, 1, 0, 1.0f);
 	}
 
 }
