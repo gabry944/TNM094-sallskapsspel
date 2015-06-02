1f6f8d1d10bff52da145879286a7654b99489c27
gabry944 <gabry944@student.liu.se>
2015-06-02 10:54:33 +0200

    instructions

--- Splash/src/com/google/sprint1/InstructionsDialog.java
+++ Splash/src/com/google/sprint1/InstructionsDialog.java
@@ -1,56 +1,70 @@
 package com.google.sprint1;
 
-import java.util.Timer;
-
-import java.util.TimerTask;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
+
 
 /**
- * Class to handle the dialog for the instructions, the dialog will dismiss after 10 seconds 
- * 
- * or on touch.
- * 
- * 
+ * Class to handle the dialog for the instructions, the dialog will dismiss on touch.
  */
  
-public class InstructionsDialog extends DialogFragment {
+public class InstructionsDialog extends DialogFragment 
+{
+	public static final String TAG = "InstructionsDialog";
+	public int counter;
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.instructionsdialog, container,
-				false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+	{
+		View rootView = inflater.inflate(R.layout.instructionsdialog, container, false);
 		
-		final Timer time = new Timer();
+		LinearLayout  displayInstructions = (LinearLayout) rootView.findViewById(R.id.instructions);
+		
+		android.graphics.drawable.Drawable d = displayInstructions.getBackground();
+		counter = 1;
 		
 		//Close dialog on touch
-		rootView.setOnClickListener(new OnClickListener() {
-		@Override
-		public void onClick(View rootView) {
-			if (getDialog() != null)
-       		 getDialog().dismiss();
-			time.cancel();
-		}
+		rootView.setOnClickListener(new OnClickListener() 
+		{
+			@Override
+			public void onClick(View rootView) {			
+				if (getDialog() != null)
+				{
+					LinearLayout  displayInstructions = (LinearLayout) rootView.findViewById(R.id.instructions);
+					if (counter == 1)
+					{
+						displayInstructions.setBackgroundResource(R.drawable.drag);
+						Log.d(TAG, "1");
+					}
+					else if (counter == 2)
+					{
+						displayInstructions.setBackgroundResource(R.drawable.hit);		
+						Log.d(TAG, "2")	;			
+					}
+					else if (counter == 3)
+					{
+						displayInstructions.setBackgroundResource(R.drawable.points);
+					}
+					else //if (counter == 4)
+					{
+						getDialog().dismiss();
+					}
+					
+					counter++;
+				}
+			}
 		});
 		
 		//No dialog title
 		getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-	
-		//Timer that closes the dialog after 10 seconds 
-        time.schedule(new TimerTask() {
-             public void run() {
-            	 if (getDialog() != null)
-            		 getDialog().dismiss();
-                 time.cancel();
-             }
-        }, 10000);
 		
 		return rootView;	
 	}
